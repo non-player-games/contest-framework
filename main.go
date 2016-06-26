1b0e1c4538d7d8d4da4061fad5c0eac1b17d1f54
Eric Liao <rcliao01@gmail.com>
2016-06-25 23:26:41 -0700

    testing compose v2 link

--- main.go
+++ main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/engine/standard"
 	"github.com/labstack/echo/middleware"
 )
 
+type User struct {
+	Name  string
+	Token string
+}
+
 func main() {
+	session, err := mgo.Dial("mongo")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	c := session.DB("test").C("people")
+	err = c.Insert(&User{"Eric", "1234"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result := User{}
+	err = c.Find(bson.M{"name": "Eric"}).One(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Token:", result.Token)
+
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
